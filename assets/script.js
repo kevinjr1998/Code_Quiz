2031a2caf3666efa7555b6536d02a3538fbf5637
Kevin98 <kevinryner@yahoo.co.uk>
2021-06-21 22:31:26 +0100

    implemented code to soft read in answers for questions, so it can be expanded

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,8 @@ question1 = {
     correctIndex: 2,
     incorrect1Index: 3,
     incorrect2index: 1,
-    incorrect3Index: 0
+    incorrect3Index: 0,
+    optionOrder: [1, 2, 0, 3]
 }
 
 questionsArray.push(question1);
@@ -48,6 +49,7 @@ function gameScore(){
     score++;
     scoreCounter.textContent = "Score: " + score;
 }
+var questionIndex = 0;
 
 var secondsLeft = 10;
 
@@ -55,67 +57,94 @@ function runGame(event){
 
     event.stopPropagation();
 
-    var questionIndex;
-
-    questionIndex =  Math.floor(Math.random() * .length);
-
-
-   
+  
 
     var questionAnswersArray = [0, 1, 2, 3];
 
     
-    questionAnswersArray[questionsArray.correctIndex] = questionsArray[questionIndex].correct;
-    questionAnswersArray[questionsArray.incorrect1Index] = questionsArray[questionIndex].incorrect1;
-    questionAnswersArray[questionsArray.incorrect2index] = questionsArray[questionIndex].incorrect2;
-    questionAnswersArray[questionsArray.incorrect3Index] = questionsArray[questionIndex].incorrect3;
+    questionAnswersArray[questionsArray[questionIndex].correctIndex] = questionsArray[questionIndex].correct;
+    questionAnswersArray[questionsArray[questionIndex].incorrect1Index] = questionsArray[questionIndex].incorrect1;
+    questionAnswersArray[questionsArray[questionIndex].incorrect2index] = questionsArray[questionIndex].incorrect2;
+    questionAnswersArray[questionsArray[questionIndex].incorrect3Index] = questionsArray[questionIndex].incorrect3;
 
 
     var questionText = document.createElement("section");
+    questionText.setAttribute("class", "answer");
     questionText.textContent = questionsArray[questionIndex].question;
     game.appendChild(questionText);
 
-    var optionArray = [option1, option2, option3, option4];
+   
+    var option1 = questionsArray[questionIndex].correctIndex;
+
+
+    option1 = document.createElement("button");
+    option1.setAttribute("class", "answer");
+    option1.textContent = questionsArray[questionIndex].correct;
+
+
+    var option2 = questionsArray[questionIndex].incorrect1Index;
+
+    option2 = document.createElement("button");
+    option2.setAttribute("class", "answer");
+    option2.textContent = questionsArray[questionIndex].incorrect1;
+
+    var option3 = questionsArray[questionIndex].incorrect2Index;
+    option3 = document.createElement("button");
+    option3.setAttribute("class", "answer");
+    option3.textContent = questionsArray[questionIndex].incorrect2;
+
+    var option4 = questionsArray[questionIndex].incorrect3Index;
+
+    option4 = document.createElement("button");
+    option4.setAttribute("class", "answer");
+    option4.textContent = questionsArray[questionIndex].incorrect3;
+
+
+    var answerOrder = [option1, option2, option3, option4];
+
+
+   
 
     for (var i = 0; i < 4; i++){
-
-    
-
+        game.appendChild(answerOrder[questionsArray[questionIndex].optionOrder[i]]);
     }
-   var option1 = document.createElement("button");
-   option1.setAttribute("id", "incorrect2");
-   option1.textContent = questionsArray[questionIndex].incorrect2;
-   game.appendChild(option1);
+  
+
+
+//    var option1 = document.createElement("button");
+//    option1.setAttribute("class", "answer");
+//    option1.textContent = questionsArray[questionIndex].incorrect2;
+//    game.appendChild(option1);
    
 
-   var option2 = document.createElement("button");
-   option2.setAttribute("id", "incorrect1");
-   option2.textContent = questionsArray[0].incorrect1;
-   game.appendChild(option2);
+//    var option2 = document.createElement("button");
+//    option2.setAttribute("id", "incorrect1");
+//    option2.textContent = questionsArray[0].incorrect1;
+//    game.appendChild(option2);
 
-   var option3 = document.createElement("button");
-   option3.setAttribute("id", "correct");
-   option3.textContent = questionsArray[0].correct;
-   game.appendChild(option3);
+//    var option3 = document.createElement("button");
+//    option3.setAttribute("id", "correct");
+//    option3.textContent = questionsArray[0].correct;
+//    game.appendChild(option3);
 
-   var option4 = document.createElement("button");
-   option4.setAttribute("id", "incorrect3");
-   option4.textContent = questionsArray[0].incorrect3;
-   game.appendChild(option4);
+//    var option4 = document.createElement("button");
+//    option4.setAttribute("id", "incorrect3");
+//    option4.textContent = questionsArray[0].incorrect3;
+//    game.appendChild(option4);
 
    
 
 
-   for (var i = 0; i < 4; i++){
-       if (optionArray[i].id == "correct"){
-           optionArray[i].addEventListener("click", function(event){
+   for (var i = 0; i < 3; i++){
+       if (questionAnswersArray[questionsArray.correctIndex] == i)  {
+           answerOrder[i].addEventListener("click", function(event){
                event.stopPropagation();
                gameScore();
                game.textContent = "";
                runGame(event);
            })   
         } else {
-            optionArray[i].addEventListener("click", function(event){ 
+            answerOrder[i].addEventListener("click", function(event){ 
             event.stopPropagation();
             secondsLeft = secondsLeft - 2;
             game.textContent = "";
